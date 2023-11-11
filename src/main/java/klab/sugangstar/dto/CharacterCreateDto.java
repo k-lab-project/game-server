f03c92e11a13060009a30c3d90a8cecf581341aa
kamothi <rhkddlf7911@naver.com>
2023-11-11 20:58:35 +0900

    add: dto

--- src/main/java/klab/sugangstar/dto/CharacterCreateDto.java
+++ src/main/java/klab/sugangstar/dto/CharacterCreateDto.java
@@ -1,2 +1,27 @@
-package klab.sugangstar.dto;public class CharacterCreateDto {
+package klab.sugangstar.dto;
+
+import klab.sugangstar.domain.*;
+import klab.sugangstar.domain.Character;
+import klab.sugangstar.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CharacterCreateDto {
+    private Long userId;
+    private int week;
+    private int stamina;
+    private int condition;
+    private String debuff1;
+    private String debuff2;
+    private String debuff3;
+    private int memorization;
+    private int concentration;
+    private int patience;
+    private int creativity;
+    private int metacognition;
+    private int understanding;
+    private List<Long> subjectIds;
 }
--- src/main/java/klab/sugangstar/dto/CharacterUpdateDto.java
+++ src/main/java/klab/sugangstar/dto/CharacterUpdateDto.java
@@ -1,2 +1,20 @@
-package klab.sugangstar.dto;public class CharacterUpdateDto {
+package klab.sugangstar.dto;
+
+import lombok.Data;
+
+@Data
+public class CharacterUpdateDto {
+    private Long characterId;
+    private int week;
+    private int stamina;
+    private int condition;
+    private String debuff1;
+    private String debuff2;
+    private String debuff3;
+    private int memorization;
+    private int concentration;
+    private int patience;
+    private int creativity;
+    private int metacognition;
+    private int understanding;
 }
