5023d7ca82d1933cb1dcb642b068df382030e895
kamothi <rhkddlf7911@naver.com>
2023-11-27 20:27:46 +0900

    add: CharacterRankSqlRepository

--- src/main/java/klab/sugangstar/repository/CharacterRankSqlRepository.java
+++ src/main/java/klab/sugangstar/repository/CharacterRankSqlRepository.java
@@ -1,2 +1,25 @@
-package klab.sugangstar.repository;public class CharacterRankSqlRepository {
+package klab.sugangstar.repository;
+
+import klab.sugangstar.domain.CharacterRankingSql;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+@Repository
+@RequiredArgsConstructor
+public class CharacterRankSqlRepository {
+    private final EntityManager em;
+
+    // 랭킹 생성
+    public void save(CharacterRankingSql rank){
+        em.persist(rank);
+    }
+
+    // 전체 삭제
+    public void deleteAll(){
+        Query query = em.createQuery("DELETE FROM CharacterRankingSql");
+        query.executeUpdate();
+    }
 }
