734bdcb9c9ebf72c0d5878d1bd9e5249f9f78f59
kamothi <rhkddlf7911@naver.com>
2024-04-08 22:54:25 +0900

    Refact: 정적 팩토리 메서드 이름 수정 및 빌더 패턴 적용

--- src/main/java/klab/sugangstar/domain/Subject.java
+++ src/main/java/klab/sugangstar/domain/Subject.java
@@ -1,6 +1,9 @@
 package klab.sugangstar.domain;
 
+import klab.sugangstar.dto.SubjectCreateDto;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -12,6 +15,7 @@ import java.util.StringTokenizer;
 @Table(name="subject")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,6 +29,23 @@ public class Subject {
     private String popularity;
     private String schedule_day;
     private String schedule_time;
+
+    @Builder
+    public Subject(Long id, String class_name, String credit, String korea_name, String english_name, String popularity, String schedule_day, String schedule_time, String star, String professor, String status1, String status2) {
+        this.id = id;
+        this.class_name = class_name;
+        this.credit = credit;
+        this.korea_name = korea_name;
+        this.english_name = english_name;
+        this.popularity = popularity;
+        this.schedule_day = schedule_day;
+        this.schedule_time = schedule_time;
+        this.star = star;
+        this.professor = professor;
+        this.status1 = status1;
+        this.status2 = status2;
+    }
+
     private String star;
     private String professor;
     // 해당 과목이 요구하는 능력치
@@ -33,22 +54,20 @@ public class Subject {
 
 
     // 생성 메서드
-    public static Subject setSubject(String class_name, String credit, String korea_name, String english_name,
-                                     String popularity, String schedule_day , String schedule_time, String star, String professor
-                                        , String status1, String status2){
-        Subject subject = new Subject();
-        subject.setClass_name(class_name);
-        subject.setCredit(credit);
-        subject.setKorea_name(korea_name);
-        subject.setEnglish_name(english_name);
-        subject.setPopularity(popularity);
-        subject.setSchedule_day(schedule_day);
-        subject.setSchedule_time(schedule_time);
-        subject.setStar(star);
-        subject.setProfessor(professor);
-        subject.setStatus1(status1);
-        subject.setStatus2(status2);
-        return subject;
+    public static Subject fromDto(SubjectCreateDto dto) {
+        return Subject.builder()
+                .class_name(dto.getClass_name())
+                .credit(dto.getCredit())
+                .korea_name(dto.getKorea_name())
+                .english_name(dto.getEnglish_name())
+                .popularity(dto.getPopularity())
+                .schedule_day(dto.getSchedule_day())
+                .schedule_time(dto.getSchedule_time())
+                .star(dto.getStar())
+                .professor(dto.getProfessor())
+                .status1(dto.getStatus1())
+                .status2(dto.getStatus2())
+                .build();
     }
 
 }
