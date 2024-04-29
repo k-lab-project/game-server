70671447a5587997b1ecb0e5bc89e0496256d44f
kamothi <rhkddlf7911@naver.com>
2024-04-29 15:28:19 +0900

    Feat: 락 설정

--- src/main/java/klab/sugangstar/repository/CharacterRankSqlRepository.java
+++ src/main/java/klab/sugangstar/repository/CharacterRankSqlRepository.java
@@ -2,9 +2,11 @@ package klab.sugangstar.repository;
 
 import klab.sugangstar.domain.CharacterRankingSql;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 import javax.persistence.Query;
 
 @Repository
@@ -13,9 +15,11 @@ public class CharacterRankSqlRepository {
     private final EntityManager em;
 
     // 랭킹 생성
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     public void save(CharacterRankingSql rank){
         em.persist(rank);
     }
+
 
     // 전체 삭제
     public void deleteAll(){
